fae295f5fc6881344ff6dc747333ed1d617e8963
Robert Zota <zotarot1@gmail.com>
2024-02-15 01:40:50 +0200

    check contents of simple folder

--- check.py
+++ check.py
@@ -1,11 +1,13 @@
 import yaml
 import json
 import os
+import shutil
+import unittest
 
 def parseYaml(file_path):
     parsed_data = {}
     with open(file_path, 'r') as file:
-         parsed_data = yaml.safe_load(file)
+        parsed_data = yaml.safe_load(file)
     return parsed_data
 
 """
@@ -54,8 +56,35 @@ def writeYaml(data_in_dict_format, file_handler, prev_indentation = ''):
             file_handler.write(f'{prev_indentation}subfolders:\n')
             for subfolder_higherarchy in value:
                 writeYaml(subfolder_higherarchy, file_handler, prev_indentation + tab_spaces)
+            
+def check_folder_contents_recursive(parent_folder_path, data_in_dict_format):
+    for key, value in data_in_dict_format.items():
+        if key == 'parent':
+            folder_name = value
+            parent_folder_path = f'{parent_folder_path}/{folder_name}'
+            if not os.path.exists(parent_folder_path):
+                return False
+        elif key == 'files':
+            list_of_file_names = value
+            for file_name in list_of_file_names:
+                file_path = f'{parent_folder_path}/{file_name}'
+                if not os.path.exists(file_path):
+                    return False
+        elif key == 'subfolders':
+            for subfolder in value:
+                pass
+                # check_folder_contents_recursive(folder_path, subfolder)
+    return True   
 
-                
+def check_folder_contents(folder_path, yaml_desc_file_path):
+    data_in_dict_format = parseYaml(yaml_desc_file_path)
+    return check_folder_contents_recursive(os.path.dirname(folder_path), data_in_dict_format[0])
+    
+def create_empty_files(folder_path, filelist):
+    for filen in filelist:
+        with open(f'{folder_path}/{filen}', 'w') as file:
+            file.write('...')
+
 # def writeYaml(data_in_dict_format, file_path):
 #     prev_indentation = ''
 #     for key, value in data_in_dict_format.items():
@@ -94,29 +123,58 @@ file_contents = """
         - timestamp.log
 """
 
-def test_should_parse_yaml_and_reprint_it():
-    file_name = 'should_parse_yaml_and_reprint_it.yaml'
-    # Write yaml to file
-    with open(file_name, 'w') as file:
-        file.write(file_contents.strip())
-    # Parse yaml from written file
-    folder_structure = parseYaml(file_name)[0]
+from pyfakefs.fake_filesystem_unittest import TestCase
 
-    # Traverse dictionary and convert back to yaml format.
-    regen_file_name = 'should_parse_yaml_and_reprint_it_regenerated.yaml'
-    with open(regen_file_name, 'w') as file:
-        writeYaml(folder_structure, file)
+class TestCaseBasic(TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
 
-    # Check that converstion from dict to yaml format worked fine.
-    assert os.path.exists(regen_file_name)
-    regen_file_contents = ''
-    with open(regen_file_name, 'r') as file:
-        regen_file_contents = file.read()
-    assert file_contents.strip() == regen_file_contents.strip()
+    def test_create_file(self):
+        file_path = "/test/file.txt"
+        self.assertFalse(os.path.exists(file_path))
+        self.fs.create_file(file_path)
+        self.assertTrue(os.path.exists(file_path))
 
-if __name__ == "__main__":
-    test_should_parse_yaml_and_reprint_it()
-    print("Everything passed")
+    def test_should_parse_yaml_and_reprint_it(self):
+        file_name = 'should_parse_yaml_and_reprint_it.yaml'
+        # Write yaml to file
+        with open(file_name, 'w') as file:
+            file.write(file_contents.strip())
+        # Parse yaml from written file
+        folder_structure = parseYaml(file_name)[0]
+
+        # Traverse dictionary and convert back to yaml format.
+        regen_file_name = 'should_parse_yaml_and_reprint_it_regenerated.yaml'
+        with open(regen_file_name, 'w') as file:
+            writeYaml(folder_structure, file)
+
+        # Check that converstion from dict to yaml format worked fine.
+        assert os.path.exists(regen_file_name) 
+        regen_file_contents = '' 
+        with open(regen_file_name, 'r') as file: 
+            regen_file_contents = file.read()
+        self.assertEqual(file_contents.strip(), regen_file_contents.strip())
+
+    def test_should_check_if_folder_contens_match_given_yaml_description(self):
+        folder_contents_description = """
+    - parent: test01
+      files:
+        - test1.txt
+        - test2.md
+    """
+        folder_path = 'res/test01'
+        if os.path.exists(folder_path):
+            shutil.rmtree(folder_path)
+        os.makedirs(folder_path)
+        create_empty_files(folder_path, ['test1.txt', 'test2.md'])
+        with open(f'{folder_path}/desc.yaml', 'w') as file:
+            file.write(folder_contents_description)
+
+        self.assertTrue(check_folder_contents(folder_path, f'{folder_path}/desc.yaml'))
+
+
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
 
 # pretty = json.dumps(prime_service, indent=4)
 # print(prime_service)
